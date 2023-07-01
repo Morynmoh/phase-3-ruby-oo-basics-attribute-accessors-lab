d350b5431851e9466d0ce51afc7739c7930b70a0
Maureen <morynmoh@gmail.com>
2023-07-01 16:54:15 +0300

    cat assignment done

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,12 @@
 ## code your solution here. 
+class Cat
+    attr_accessor :name
+    def meaw
+        puts "meow"
+    end
+    
+end
+cat = Cat.new
+cat.name = "Whiskers"
+puts cat.name # Output: Whiskers
+cat.meaw 
